fc78af821cd470d66468c8fade960a8db5100717
Rob Pickering <rob@pickering.org>
2024-11-07 19:21:42 +0000

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # secretenv
-Really simple wrapper around dotenv which allows single aggregate keys
+Really simple wrapper around dotenv which allows encrypted bundled environment variable sets to be loaded into the environment.
+
+```
+Normal .env file loading into `process.env` occurs
+If `SECRETENV_KEY` is set (in environment, or .env), then:
+  If `SECRETENV_VALUE` is set, then:
+    `SECRETENV_VALUE` is decrypted using `SECRETENV_KEY` and the JSON contents destructured on top of `process.env`
+  else If `SECRETENV_VALUE` is not set, then:
+    the parsed contents of the .env file are dumped into a JSON structure and encrypted using `SECRETENV_KEY` then output to the console in a format that can be used to set a single `SECRETENV_VALUE` environment variable.
+```
+
+## Usage
---
+++ index.js
@@ -0,0 +1,45 @@
+const dotenv = require('dotenv');
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+
+function config() {
+  const { parsed } = dotenv.config(...arguments);
+  const { SECRETENV_KEY, SECRETENV_VALUE } = process.env;
+  const IV_LENGTH = 16;
+  let key = false;
+  if (SECRETENV_KEY) {
+    const hmac = crypto.createHmac('sha256', 'secretenv');
+    hmac.update(SECRETENV_KEY);
+    key = hmac.digest();
+  }
+
+  function encrypt(text) {
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('base64');
+  }
+
+  function decrypt(text) {
+    let textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'base64');
+    let decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  }
+
+  if (key) {
+    if (Object.keys(parsed).length > 0 && !key) {
+      console.log(`SECRETENV_VALUE=${encrypt(JSON.stringify(parsed))}`);
+    }
+    else if (key) {
+      const composite_value = decrypt(SECRETENV_VALUE);
+      Object.assign(process.env, JSON.parse(composite_value));
+    }
+  }
+}
+
+module.exports = { ...dotenv, config };
